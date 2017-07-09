acc76d2b907308265f4d22a0711f6ecf384adad8
Jones <n0250388@lm.lmig.com>
2017-07-08 22:11:39 -0400

    Lucas upload

---
+++ java8stream-exercises/src/main/java/com/technologyconversations/java8exercises/streams/FilterCollection.java
@@ -0,0 +1,27 @@
+package com.technologyconversations.java8exercises.streams;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
+public class FilterCollection {
+
+    private FilterCollection() {
+    }
+
+    public static List<String> transform7(List<String> collection) {
+        List<String> newCollection = new ArrayList<>();
+        for (String element : collection) {
+            if (element.length() < 4) {
+                newCollection.add(element);
+            }
+        }
+        return newCollection;
+    }
+
+    public static List<String> transform(List<String> collection) {
+        return null;
+    }
+
+}
